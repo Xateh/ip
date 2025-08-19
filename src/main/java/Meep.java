2cbda1700456c8df9467748a886a4bc314c39a68
Xateh <155933957+Xateh@users.noreply.github.com>
2025-08-19 16:38:58 +0800

    Meep-skeleton

--- src/main/java/Meep.java
+++ src/main/java/Meep.java
@@ -1,5 +1,22 @@
 public class Meep {
-    public static void main(String[] args) {
+    private static void printBorder() {
+        System.out.println("-".repeat(50));
+    }
+
+    private static void printGreeting() {
         System.out.println("Hello from Meep!");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void printFarewell() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        printBorder();
+        printGreeting();
+
+        printBorder();
+        printFarewell();
     }
 }
