cd2921e2003d7c8b826617e0546051e024b913fc
Xateh <155933957+Xateh@users.noreply.github.com>
2025-09-06 03:39:14 +0000

    update GUI to JavaFX tutorial part 4

--- src/main/java/meep/gui/DialogBox.java
+++ src/main/java/meep/gui/DialogBox.java
@@ -9,39 +9,52 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
+import java.util.Collections;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+/**
+ * Represents a dialog box consisting of an ImageView to represent the speaker's face
+ * and a label containing text from the speaker.
+ */
 public class DialogBox extends HBox {
+    @FXML
+    private Label dialog;
+    @FXML
+    private ImageView displayPicture;
 
-	private Label text;
-	private ImageView displayPicture;
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	private DialogBox(String s, Image i) {
-		text = new Label(s);
-		displayPicture = new ImageView(i);
+        dialog.setText(text);
+        displayPicture.setImage(img);
+    }
 
-		text.setWrapText(true);
-		displayPicture.setFitWidth(100.0);
-		displayPicture.setFitHeight(100.0);
-		this.setAlignment(Pos.TOP_RIGHT);
-
-		this.getChildren().addAll(text, displayPicture);
-	}
-
-	/**
+    /**
      * Flips the dialog box such that the ImageView is on the left and text on the right.
      */
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
-    public static DialogBox getUserDialog(String s, Image i) {
-        return new DialogBox(s, i);
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
     }
 
-    public static DialogBox getMeepDialog(String s, Image i) {
-        var db = new DialogBox(s, i);
+    public static DialogBox getMeepDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
         db.flip();
         return db;
     }
